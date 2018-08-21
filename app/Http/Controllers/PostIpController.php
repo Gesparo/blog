b63cd8789935eddaceb0ea69f666d41928269993
Melnik Maksim <shmaxer@rambler.ru>
2018-08-21 19:38:25 +0500

    Refactor sql query

--- app/Http/Controllers/PostIpController.php
+++ app/Http/Controllers/PostIpController.php
@@ -35,17 +35,14 @@ class PostIpController extends Controller
         $result = DB::select(DB::raw('
             SELECT posts.user_ip as ip, users.login as login
             FROM posts
-              JOIN users
-                ON users.id = posts.user_id
-              JOIN (
-                SELECT user_ip
-                FROM (
-                       SELECT user_ip, COUNT(*) as users_ip_count
-                       FROM posts
-                       GROUP BY user_ip
-                 ) as counted_ips
-                WHERE users_ip_count > 1
-                ) as result_table
+            JOIN users
+              ON users.id = posts.user_id
+            JOIN (
+                   SELECT user_ip
+                   FROM posts
+                   GROUP BY user_ip
+                   HAVING COUNT(*) > 1
+                 ) as result_table
               ON result_table.user_ip = posts.user_ip
             ORDER BY posts.user_ip
         '));
